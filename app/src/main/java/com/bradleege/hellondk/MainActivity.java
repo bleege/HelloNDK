1d076066c8df0cbc0c5ad922cd4e565d5db8a3a6
Brad Leege <bleege@gmail.com>
2015-06-16 15:53:02 -0500

    Android button for calling JNI code

--- app/src/main/java/com/bradleege/hellondk/MainActivity.java
+++ app/src/main/java/com/bradleege/hellondk/MainActivity.java
@@ -2,12 +2,29 @@ package com.bradleege.hellondk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        System.loadLibrary("hellondklibrary");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.callNativeButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                helloLog("Foo!");
+            }
+        });
+
     }
+
+    private native void helloLog(String logThis);
 }
